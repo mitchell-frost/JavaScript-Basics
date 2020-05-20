766e465e9ed06f3fcded577494637b83f9c93bfc
Bhawna Patnaik <44670836+mitchell-frost@users.noreply.github.com>
2020-05-20 17:22:38 +0530

    Add files via upload

---
+++ Age Calculator/age.js
@@ -0,0 +1,2 @@
+var age=prompt("What is your age?");
+alert("your age is "+ 365.25*age);
---
+++ Name exercise/script.js
@@ -0,0 +1,5 @@
+var firstName=prompt("WHat is your first name?");
+var secondName=prompt("WHat is your last name?");
+var age=prompt("WHat is your age?");
+console.log("Your full name is "+firstName+" "+secondName);
+console.log("Your age is "+age);
